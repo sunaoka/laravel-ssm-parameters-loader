e6b5fa06a1b1d0ff9c429986a2d105cb3c4fb837
sunaoka <sunaoka@pocari.org>
2024-01-18 11:11:46 +0900

    Running Pint

--- src/ParametersLoader.php
+++ src/ParametersLoader.php
@@ -16,7 +16,7 @@ class ParametersLoader
     ) {
     }
 
-    public function load(string $prefix = null): void
+    public function load(?string $prefix = null): void
     {
         $parameters = $this->getParameters($prefix ?? $this->prefix);
 
@@ -31,7 +31,7 @@ class ParametersLoader
      *
      * @link https://docs.aws.amazon.com/systems-manager/latest/APIReference/API_GetParameters.html
      */
-    public function getParameters(string $prefix = null): array
+    public function getParameters(?string $prefix = null): array
     {
         return cache()->remember('ssm-parameters-loader', $this->ttl, function () use ($prefix): array {  // @phpstan-ignore-line
             $environments = $this->getEnvironments($prefix ?? $this->prefix);
@@ -43,7 +43,7 @@ class ParametersLoader
             foreach (array_chunk($environments, 10, true) as $names) {
                 /** @var array{Parameters: array<int, array{Name: string, Value: string}>, InvalidParameters: string[]} $result */
                 $result = $this->ssmClient->getParameters([
-                    'Names'          => array_values($names),
+                    'Names' => array_values($names),
                     'WithDecryption' => true,
                 ]);
                 foreach ($result['Parameters'] as $parameter) {
@@ -56,7 +56,7 @@ class ParametersLoader
 
             $invalidParameters = array_merge(...$invalidParameters);
             if (count($invalidParameters) > 0) {
-                throw new RuntimeException('Invalid AWS Systems Manager parameter store names: ' . implode(', ', $invalidParameters));
+                throw new RuntimeException('Invalid AWS Systems Manager parameter store names: '.implode(', ', $invalidParameters));
             }
 
             return $parameters;
@@ -69,15 +69,15 @@ class ParametersLoader
     protected function getEnvironments(string $prefix): array
     {
         $environments = getenv(local_only: true);  // @phpstan-ignore-line
-        if (!is_array($environments)) {
+        if (! is_array($environments)) {
             return [];  // @codeCoverageIgnore
         }
 
-        $environments = array_filter($environments, static fn(string $value): bool => str_starts_with($value, $prefix));
+        $environments = array_filter($environments, static fn (string $value): bool => str_starts_with($value, $prefix));
         if (count($environments) === 0) {
             return [];
         }
 
-        return array_map(static fn(string $value): string => substr($value, strlen($prefix)), $environments);
+        return array_map(static fn (string $value): string => substr($value, strlen($prefix)), $environments);
     }
 }
